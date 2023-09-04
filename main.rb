47ab4707bb388226de0dd5eebcddc3550928e4be
olanike olatunji <olanikegloria2020@gmail.com>
2023-09-04 13:21:25 +0100

    Refactored code

--- main.rb
+++ main.rb
@@ -1,43 +1,35 @@
 require_relative 'app'
 
-# class Show
-#   def initialize(app)
-#     @app = app
-#   end
-
-#   def choose_number(app, choice)
-#     case choice
-#     when 1
-#       app.list__all_books
-#     when 2
-#       app.list_all_persons
-#     when 3
-#       app.create_a_person
-#     when 4
-#       app.create_a_book
-#     when 5
-#       app.create_a_rental
-#     when 6
-#       app.list_of_rentals
-#     when 7
-#       puts 'Good bye'
-#     end
-#   end
-
-#   def start
-#     loop do
-#       render_choices
-#       choice = gets.chomp.to_i
-#       break if choice == 7
-
-#       choose_number(@app, choice)
-#     end
-#   end
-# end
-
+def render_choices
+  puts 'Please Choose an Option by entering a number:'
+  puts '1. List all books.'
+  puts '2. List all people.'
+  puts '3. Create a person.'
+  puts '4. Create a book.'
+  puts '5. Create a rental.'
+  puts '6. List all rentals for a given person id.'
+  puts '7. EXIT'
+end
+def choose_number(choice, app)
+  case choice
+  when 1 then app.list__all_books
+  when 2 then app.list_all_persons
+  when 3 then app.create_a_person
+  when 4 then app.create_a_book
+  when 5 then app.create_a_rental
+  when 6 then app.list_of_rentals
+  end
+end
 def main
   app = App.new
-  app.show
+  loop do
+    render_choices
+    choice = gets.chomp.to_i
+    if choice >= 7
+      puts 'Thank you for using this app'
+      break
+    end
+    choose_number(choice, app)
+  end
 end
-
 main
