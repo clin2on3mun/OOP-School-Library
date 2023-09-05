270708d7bba6a86d384956d9d8b53b882124633f
olanike olatunji <olanikegloria2020@gmail.com>
2023-09-05 19:14:59 +0100

    Made changes in preserve_data.rb

--- lib/preserve_data.rb
+++ lib/preserve_data.rb
@@ -1,1 +1,20 @@
-
+class PreserveData
+    def create_student_data(person, person_type )
+        {
+            person_type: person_type,
+            id: person.id,
+            age: person.age,
+            name: person.name,
+            parent_permission: person.parent_permission,
+        }
+    end
+    def create_teacher_data(person, person_type)
+        {
+            person_type: person_type,
+            id: person.id,
+            age: person.age,
+            name: person.name,
+            specialization: person.specialization,
+        }
+    end
+end
